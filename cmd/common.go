8c92045951fb4a86227f1863b563c3c1afe12ac8
Miguel Villar <mvillarjuan@gmail.com>
2022-04-28 22:43:38 +0000

    :white_check_mark: TestGetSessionIdWithoutError

--- cmd/common.go
+++ cmd/common.go
@@ -1,8 +1,23 @@
 package cmd
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+)
 
 type Data struct {
+}
+
+type ApiLoginResponse struct {
+	Data string `json:"data"`
+	Meta struct {
+		CurrentPage int `json:"currentPage"`
+		LastPage    int `json:"lastPage"`
+		Total       int `json:"total"`
+		PerPage     int `json:"perPage"`
+	} `json:"meta"`
 }
 
 type External interface {
@@ -10,5 +25,21 @@ type External interface {
 }
 
 func getSesionId(sesameUrl, sesameUsername, sesamePassword string) (string, error) {
-	return "", nil
+	values := map[string]string{"foo": "bar"}
+	jsonData, _ := json.Marshal(values)
+
+	req, _ := http.NewRequest(http.MethodPost, sesameUrl+"/api/v3/security/login", bytes.NewBuffer(jsonData))
+	req.Header.Add("Accept", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	post := &ApiLoginResponse{}
+	derr := json.NewDecoder(res.Body).Decode(post)
+	if derr != nil {
+		panic(derr)
+	}
+	return post.Data, nil
 }
